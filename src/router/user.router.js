15430fcff5a3dc9df226092928b9cc3934fbdf0a
Jin Yi <xxmyj.k@gmail.com>
2016-12-26 18:01:02 +0800

    补充登录注册参数判断逻辑

--- src/router/user.router.js
+++ src/router/user.router.js
@@ -8,11 +8,39 @@ router.get('/', (req, res, next) => {
 });
 
 router.post('/register', (req, res, next) => {
+    var param = {
+        nickname: req.body.nickname,
+        email: req.body.email,
+        mobile: req.body.mobile,
+        password: req.body.password,
+    };
 
+    if (!param.nickname || !param.email || !param.mobile || !param.password) {
+        return res.send('param nickname, email, mobile, password is required.');
+    }
+
+    user_action.register(param).then(rs => {
+        return res.json(rs);
+    }).catch(err => {
+        return res.json(err);
+    });
 });
 
 router.post('/login', (req, res, next) => {
+    var param = {
+        username: req.body.username,
+        password: req.body.password
+    };
 
+    if (!param.username || !param.password) {
+        return res.send('param username, password is required.');
+    }
+
+    user_action.login(param).then(rs => {
+        return res.json(rs);
+    }).catch(err => {
+        return res.json(err);
+    });
 });
 
 module.exports = router;
