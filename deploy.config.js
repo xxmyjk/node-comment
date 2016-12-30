75e372463833d49a8a1932a64b8aee03f4ec7561
Jin Yi <xxmyj.k@gmail.com>
2016-12-30 17:46:59 +0800

    添加pm2 deploy 配置文件

---
+++ deploy.config.js
@@ -0,0 +1,46 @@
+module.exports = {
+  /**
+   * Application configuration section
+   * http://pm2.keymetrics.io/docs/usage/application-declaration/
+   */
+    apps : [
+
+        // First application
+        {
+            name      : 'node-comment',
+            script    : 'index.js',
+            env: {
+                COMMON_VARIABLE: 'true'
+            },
+            env_production : {
+                NODE_ENV: 'production'
+            }
+        },
+    ],
+
+  /**
+   * Deployment section
+   * http://pm2.keymetrics.io/docs/usage/deployment/
+   */
+    deploy : {
+        production : {
+            user : 'work',
+            host : '60.205.141.126',
+            ref  : 'origin/master',
+            repo : 'git@github.com:xxmyjk/node-comment.git',
+            path : '/mnt/deploy_root/producction',
+            'post-deploy' : 'npm install && pm2 startOrRestart ecosystem.json --env production'
+        },
+        dev : {
+            user : 'work',
+            host : '60.205.141.126',
+            ref  : 'origin/master',
+            repo : 'git@github.com:xxmyjk/node-comment.git',
+            path : '/mnt/deploy_root/development',
+            'post-deploy' : 'npm install && pm2 startOrRestart ecosystem.json --env dev',
+            env  : {
+                NODE_ENV: 'dev'
+            }
+        }
+    }
+};
